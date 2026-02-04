ba834c1bf567defb969712512d21b33b31091c92
Leandro Tombetta <leandro.tombetta87@gmail.com>
2026-02-04 10:12:00 -0300

    add: ports document and embeddings, also i create document entity

--- application/ports/embedding_port.py
+++ application/ports/embedding_port.py
@@ -1,2 +1,32 @@
-# Embedding port interface
-pass
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class EmbeddingPort(ABC):
+    """Port for generating text embeddings"""
+    
+    @abstractmethod
+    def embed(self, text: str) -> List[float]:
+        """
+        Generate embedding vector for given text
+        
+        Args:
+            text: Text to embed
+            
+        Returns:
+            List of floats representing the embedding vector
+        """
+        pass
+    
+    @abstractmethod
+    def embed_batch(self, texts: List[str]) -> List[List[float]]:
+        """
+        Generate embeddings for multiple texts
+        
+        Args:
+            texts: List of texts to embed
+            
+        Returns:
+            List of embedding vectors
+        """
+        pass
